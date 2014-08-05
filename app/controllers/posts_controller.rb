6282cce86897d9e3749a4442cc365f642b45d7b8
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-05 15:11:24 -0400

    Redirect users back posts page if they try to go to a post that does not exist

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+	before_action :set_post, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@posts = Post.all
 	end
@@ -20,15 +22,12 @@ class PostsController < ApplicationController
 	end
 
 	def show
-		@post = Post.find(params[:id])
 	end
 
 	def edit
-		@post = Post.find(params[:id])
 	end
 
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(post_params)
 			flash[:notice] = "Post has been updated."
 			redirect_to @post
@@ -39,7 +38,6 @@ class PostsController < ApplicationController
 	end
 
 	def destroy
-		@post = Post.find(params[:id])
 		@post.destroy
 
 		flash[:notice] = "Post has been deleted."
@@ -52,4 +50,10 @@ class PostsController < ApplicationController
 		params.require(:post).permit(:title, :content)
 	end
 
+	def set_post
+		@post = Post.find(params[:id])
+	rescue ActiveRecord::RecordNotFound
+		flash[:alert] = "The post you were looking for could not be found."
+		redirect_to posts_path
+	end
 end
