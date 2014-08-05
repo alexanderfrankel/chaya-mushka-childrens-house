cc7e22059839fb53c5ffd3d4d83bfb61ae6ff23c
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-05 14:56:16 -0400

    Add ability to update a post

---
+++ spec/features/editing_posts_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature "Editing Posts" do
+
+	before do
+		create(:post, title: "Example Post")
+
+		visit '/'
+		click_link "Example Post"
+		click_link "Edit Post"
+	end
+
+	scenario "Updating a post" do
+		fill_in "Title", with: "Example Post beta"
+		click_button "Update Post"
+
+		expect(page).to have_content("Post has been updated.")
+	end
+
+	scenario "Updating a post with invalid attributes" do
+		fill_in "Title", with: ""
+		click_button "Update Post"
+
+		expect(page).to have_content("Post has not been updated.")
+	end
+end
