7d9f5a415e9f09c660a047157943edcee272c230
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-08 19:09:59 -0400

    Add faculty user to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,11 @@ User.create(first_name: "alex",
 						password: "password",
 						password_confirmation: "password",
 						shutterfly_url: "does not exist")
+
+User.create(first_name: "alex",
+						last_name: "frank",
+						email: "alex.frankel@gmail.com",
+						password: "password",
+						password_confirmation: "password",
+						shutterfly_url: "does not exist",
+						faculty: true)
