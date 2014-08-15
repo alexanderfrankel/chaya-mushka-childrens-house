0af21a442649c11c7b9c0b0778816fae4d073076
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-14 21:53:19 -0400

    Add full testing coverage of all actions in Admin::Users controller

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -7,20 +7,32 @@ describe Admin::UsersController do
 	context "standard users" do
 		before { sign_in(user) }
 
-		it "are not able to access the index action" do
-			get 'index'
-			expect(response).to redirect_to('/')
-			expect(flash[:alert]).to eql("You must be an admin to do that.")
-		end
+		{ new: :get,
+		  create: :post,
+		  edit: :get,
+		  update: :put,
+		  destroy: :delete }.each do |action, method|
+		  	it "cannot access the #{action} action" do
+		  		send(method, action, :id => create(:post))
+		  		expect(response).to redirect_to(root_path)
+		  		expect(flash[:alert]).to eql("You must be an admin to do that.")
+		  	end
+		  end
 	end
 
 	context "faculty users" do
 		before { sign_in(faculty_user) }
 
-		it "are not able to access the index action" do
-			get 'index'
-			expect(response).to redirect_to('/')
-			expect(flash[:alert]).to eql("You must be an admin to do that.")
-		end
+		{ new: :get,
+		  create: :post,
+		  edit: :get,
+		  update: :put,
+		  destroy: :delete }.each do |action, method|
+		  	it "cannot access the #{action} action" do
+		  		send(method, action, :id => create(:post))
+		  		expect(response).to redirect_to(root_path)
+		  		expect(flash[:alert]).to eql("You must be an admin to do that.")
+		  	end
+		  end
 	end
 end
