b936caf1481d6ad1170ac4c64105749a97623f07
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-14 21:58:32 -0400

    Error handling spec for missing user

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Admin::UsersController do
 	let(:user) { create(:user) }
 	let(:faculty_user) { create(:faculty_user) }
+	let(:admin_user) { create(:admin_user) }
 
 	context "standard users" do
 		before { sign_in(user) }
@@ -34,5 +35,19 @@ describe Admin::UsersController do
 		  		expect(flash[:alert]).to eql("You must be an admin to do that.")
 		  	end
 		  end
+	end
+
+	context "admin users" do
+		before do
+			sign_in(admin_user)
+		end
+
+		it "displays an error for a missing user" do
+			get :show, id: "not-here"
+
+			expect(response).to redirect_to(admin_users_path)
+			message = "The user you were looking for could not be found."
+			expect(flash[:alert]).to eql(message)
+		end
 	end
 end
