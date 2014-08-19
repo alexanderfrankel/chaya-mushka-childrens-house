51c9ad33dc1f69b2f1004bf4161ce05314debb6c
Alexander Frankel <alexxander.frankel@gmail.com>
2014-08-19 13:10:54 -0400

    Add uniqueness validation on Tuition model

--- spec/models/tuition_spec.rb
+++ spec/models/tuition_spec.rb
@@ -14,4 +14,38 @@ describe Tuition do
 			expect(t).to be_valid
 		end
 	end
+
+	describe "years" do
+		it "must be unique" do
+			t = Tuition.new(year: "2014 - 2015",
+			 								amount: 1000)
+			t.save
+			expect(t).to be_valid
+
+			y = Tuition.new(year: "2014 - 2015",
+			 								amount: 3000)
+			y.save
+			expect(y).to_not be_valid
+		end
+	end
+
+	describe "current status" do
+		it "must be the only tuition that is current" do
+			t = Tuition.new(year: "2014 - 2015",
+			 								amount: 2000,
+			 								current: true)
+			t.save
+			expect(t).to be_valid
+
+			y = Tuition.new(year: "2015 - 2016",
+			 								amount: 6000,
+			 								current: true)
+			y.save
+			expect(y).to_not be_valid
+
+			y.current = false
+			y.save
+			expect(y).to be_valid
+		end
+	end
 end
