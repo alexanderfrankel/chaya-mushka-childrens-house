979ff0644dc6fbe974640328d27c5b026e743f69
Alexander Frankel <alexxander.frankel@gmail.com>
2014-09-17 18:48:36 -0400

    Fix session#create if no user is found when signing in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,29 @@
 class SessionsController < ApplicationController
-	before_action :check_user_verification, only: :create
 
 	def new
 	end
 
 	def create
 		user = User.where(:email => params[:signin][:email]).first
-		if user && user.authenticate(params[:signin][:password])
-			if params[:remember_me]
-				cookies.permanent[:auth_token] = user.auth_token
+
+		if user
+			if user.verified?
+				if user.authenticate(params[:signin][:password])
+					if params[:remember_me]
+						cookies.permanent[:auth_token] = user.auth_token
+					else
+						cookies[:auth_token] = user.auth_token
+					end
+					flash[:notice] = "Signed in successfully."
+					redirect_to root_url
+				else
+					flash[:error] = "Sorry. Your email or password is invalid."
+					render :new
+				end
 			else
-				cookies[:auth_token] = user.auth_token
+				flash[:error] = "Apologies. Your account has not yet been verified by the administrative office. Please wait for verification email."
+				redirect_to root_url
 			end
-			flash[:notice] = "Signed in successfully."
-			redirect_to root_url
 		else
 			flash[:error] = "Sorry. Your email or password is invalid."
 			render :new
@@ -25,15 +35,5 @@ class SessionsController < ApplicationController
 		flash[:notice] = "Signed out successfully."
 
 		redirect_to root_url
-	end
-
-	private
-
-	def check_user_verification
-		user = User.where(:email => params[:signin][:email]).first
-		unless user.verified?
-			flash[:error] = "Apologies. Your account has not yet been verified by the administrative office. Please wait for verification email."
-			redirect_to root_url
-		end
 	end
 end
