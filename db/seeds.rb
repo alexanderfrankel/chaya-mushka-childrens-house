d94cfd9e8665d2b3ae7f1b91ff671c51caa95295
Alexander Frankel <alexxander.frankel@gmail.com>
2014-09-19 17:57:21 -0400

    Load upcoming events into homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -55,6 +55,72 @@ event2 = Event.create(title: "Event 2",
 											end_date: "2015-07-04 00:00:00",
 											user: admin)
 
+event3 = Event.create(title: "Event 3",
+											description: "Event 3's description goes here.",
+											start_date: "2014-10-03 00:00:00",
+											end_date: "2014-10-04 00:00:00",
+											user: admin)
+
+event4 = Event.create(title: "Event 4",
+											description: "Event 4's description goes here.",
+											start_date: "2014-10-10 00:00:00",
+											end_date: "2014-10-11 00:00:00",
+											user: admin)
+
+event5 = Event.create(title: "Event 5",
+											description: "Event 5's description goes here.",
+											start_date: "2014-11-03 00:00:00",
+											end_date: "2014-11-04 00:00:00",
+											user: admin)
+
+event6 = Event.create(title: "Event 6",
+											description: "Event 6's description goes here.",
+											start_date: "2014-11-20 00:00:00",
+											end_date: "2014-11-21 00:00:00",
+											user: admin)
+
+event7 = Event.create(title: "Event 7",
+											description: "Event 7's description goes here.",
+											start_date: "2014-12-03 00:00:00",
+											end_date: "2014-12-04 00:00:00",
+											user: admin)
+
+event8 = Event.create(title: "Event 8",
+											description: "Event 8's description goes here.",
+											start_date: "2014-12-30 00:00:00",
+											end_date: "2014-12-31 00:00:00",
+											user: admin)
+
+event9 = Event.create(title: "Event 9",
+											description: "Event 9's description goes here.",
+											start_date: "2015-02-03 00:00:00",
+											end_date: "2015-02-04 00:00:00",
+											user: admin)
+
+event10 = Event.create(title: "Event 10",
+											description: "Event 10's description goes here.",
+											start_date: "2015-04-03 00:00:00",
+											end_date: "2015-04-04 00:00:00",
+											user: admin)
+
+event11 = Event.create(title: "Event 11",
+											description: "Event 11's description goes here.",
+											start_date: "2014-10-01 00:00:00",
+											end_date: "2014-10-02 00:00:00",
+											user: admin)
+
+event12 = Event.create(title: "Event 12",
+											description: "Event 12's description goes here.",
+											start_date: "2014-11-29 00:00:00",
+											end_date: "2014-11-30 00:00:00",
+											user: admin)
+
+event13 = Event.create(title: "Event 13",
+											description: "Event 13's description goes here.",
+											start_date: "2014-12-01 00:00:00",
+											end_date: "2014-12-02 00:00:00",
+											user: admin)
+
 # TUITIONS ----------------------------------------
 
 tuition1 = Tuition.create(year: "2013 - 2014",
